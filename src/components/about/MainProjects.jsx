97c054f4b283f21b367dab4e83269839310d6ad0
Bapparaj sk <bapparajsk97@gmail.com>
2024-12-23 19:51:54 +0530

    feat(about): enhance MainProjects component with new animations and update ImponentText styling

--- src/components/about/MainProjects.jsx
+++ src/components/about/MainProjects.jsx
@@ -1,8 +1,9 @@
 "use client";
 
-import React, { useEffect, useRef, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import gsap from "gsap";
 import { ScrollTrigger } from "gsap/all";
+import { ImponentText } from '../ui/imponent-text';
 
 gsap.registerPlugin(ScrollTrigger);
 
@@ -51,7 +52,7 @@ const MainProjects = () => {
 
   function loadImage(idx) {
     if (idx < 0 || idx > frame.current.maxIdx) {
-      return;      
+      return;
     }
 
     const canvas = canvasRef.current;
@@ -86,7 +87,7 @@ const MainProjects = () => {
       scrollTrigger: {
         trigger: '.parent',
         start: 'top bottom',
-        end: 'bottom top',
+        end: 'bottom bottom',
         scrub: 2,
         anticipatePin: 1,
       },
@@ -103,51 +104,55 @@ const MainProjects = () => {
     }
 
     tl
-    .to(frame.current, anime(50), "fast")
-    .to(".animate-1", { opacity: 1 }, "fast")
+      .to(frame.current, anime(50), "fast")
+      .to(".animate-1", { opacity: 1 }, "fast")
 
-    .to(frame.current, anime(100), "second")
-    .to(".animate-1", { y: -50, opacity: 0 }, "second")
+      .to(frame.current, anime(100), "second")
+      .to(".animate-1", { y: -50, opacity: 0 }, "second")
 
-    .to(frame.current, anime(150), "thard")
-    .to(".animate-2", { y: -50, opacity: 1 }, "thard")
+      .to(frame.current, anime(150), "thard")
+      .to(".animate-2", { y: -50, opacity: 1 }, "thard")
 
-    .to(frame.current, anime(200), "forth")
-    .to(".animate-2", { opacity: 1 }, "forth")
+      .to(frame.current, anime(200), "forth")
+      .to(".animate-2", { opacity: 1 }, "forth")
 
-    .to(frame.current, anime(250), "fifth")
-    .to(".animate-2", { y: -100, opacity: 0 }, "fifth")
+      .to(frame.current, anime(250), "fifth")
+      .to(".animate-2", { y: -100, opacity: 0 }, "fifth")
 
-    .to(frame.current, anime(300), "ninth")
-    .to(".animate-4", { x: "0%", ease: "linear" }, "ninth")
+      .to(frame.current, anime(270), "ninth")
+      .to(".animate-4-bottom", { opacity: 1, y: -50 }, "ninth")
+      .to(".animate-4-top", { opacity: 1, y: 50 }, "ninth")
 
-    .to(frame.current, anime(350), "tenth")
-    .to(".animate-4", { x: "0%" }, "tenth")
+      .to(frame.current, anime(350), "tenth")
+      .to(".animate-4-bottom", { opacity: 1, }, "tenth")
+      .to(".animate-4-top", { opacity: 1, }, "tenth")
 
-    .to(frame.current, anime(400), "eleventh")
-    .to(".animate-4", { x: "0%", opacity: "0", ease: "circ.inOut" }, "eleventh")
+      .to(frame.current, anime(430), "eleventh")
+      .to(".animate-4-bottom", { opacity: 0, y: -100, ease: "linear" }, "eleventh")
+      .to(".animate-4-top", { opacity: 0, y: 100, ease: "linear" }, "eleventh")
 
-    .to(frame.current, anime(450), "twelfth")
-    .to(".animate-5", { opacity: 1 }, "twelfth")
+      .to(frame.current, anime(450), "twelfth")
+      .to(".animate-5", { opacity: 1 }, "twelfth")
 
-    .to(frame.current, anime(500), "thirteenth")
-    .to(".animate-5", { opacity: 1 }, "thirteenth")
-    .to("canvas", { scale: 0.7,  ease: "circ.inOut" }, "thirteenth")
+      .to(frame.current, anime(500), "thirteenth")
+      .to(".animate-5", { opacity: 1 }, "thirteenth")
+      .to("canvas", { scale: 0.7, ease: "circ.inOut", borderRadius: 30 }, "thirteenth")
 
-    .to(frame.current, anime(550), "fourteenth")
-    .to(".animate-5", { opacity: 1 }, "fourteenth")
-    .to("canvas", { scale: 1,  ease: "linear" }, "fourteenth")
+      .to(frame.current, anime(550), "fourteenth")
+      .to(".animate-5", { opacity: 1 }, "fourteenth")
+      .to("canvas", { scale: 0.7, ease: "circ.inOut", borderRadius: 30 }, "fourteenth")
+      .to(".animate-5 span", { width: 100, ease: "elastic" }, "fourteenth")
 
-    .to(frame.current, anime(600), "fifteenth")
-    .to(".animate-5 span", { width: 100, ease: "elastic" }, "fifteenth")
+      .to(frame.current, anime(600), "fifteenth")
+      .to(".animate-5", { opacity: 1 }, "fifteenth")
+      .to("canvas", { scale: 1, ease: "linear", borderRadius: 0 }, "fifteenth")
 
-    .to(frame.current, anime(650), "sixteenth")
-    .to(".animate-5 span", { width: 20, ease: "elastic" }, "sixteenth")
+      .to(frame.current, anime(650), "sixteenth")
 
-    .to(frame.current, anime(703), "seventeenth")
-    .to(".animate-5 span", { opacity: 0 }, "seventeenth")
+      .to(frame.current, anime(703), "seventeenth")
+      .to(".animate-5 span", { width: 100, ease: "elastic" }, "seventeenth")
   }
-
+ 
   return (
     <div className="parent relative w-full h-[1800vh] ">
       <div className="w-full sticky top-0 left-0 h-screen bg-cover bg-center bg-no-repeat overflow-x-hidden">
@@ -157,18 +162,38 @@ const MainProjects = () => {
           ref={canvasRef}
         />
         <div className='absolute bottom-24 left-24 animate-1'>
-          <h2 className='text-neutral-800 text-6xl font-bold font-ubuntu '>
-            I am a Software Engineer
+          <h2 className='text-neutral-800 text-6xl font-ubuntu '>
+            Welcome to my About
           </h2>
         </div>
         <div className='absolute bottom-24 right-24 animate-2 opacity-0'>
-          <h2 className='text-neutral-800 text-6xl font-bold font-ubuntu '>
-            I am a Software Engineer
+          <h2 className='text-neutral-800 text-6xl font-ubuntu '>
+            start a new <span className="bg-[linear-gradient(90deg,#0D92F4_0%,#6256CA_40%,#F95454_100%)] text-transparent bg-clip-text font-bold">journey</span>
           </h2>
         </div>
-        <div className='absolute bottom-0 right-0 h-full w-1/4 bg-black translate-x-full animate-4'></div>
+        <div className='absolute top-10 left-20 max-w-[600px] animate-4-top opacity-0 bg-white p-4 rounded-lg'>
+          <p className="text-left  text-neutral-800 text-xl">
+            I'm a passionate software engineer skilled in <ImponentText
+              words={"Java, JavaScript, TypeScript,"} /> ans <ImponentText words={"Node.js"} />. I specialize in
+            full-stack development, working with frameworks like <ImponentText
+              words={"React.js, Next.js, Express, Tailwind CSS, SASS, "} />, ans <ImponentText
+              words={"Framer Motion"} /> .
+            My experience includes working with <ImponentText
+              words={"AWS, Google Cloud, Firebase, BullMQ,"} />and <ImponentText words={"MongoDB"} />.
+            My experience spans across <ImponentText
+              words={"AWS, Google Cloud, Firebase, BullMQ,"} /> ans <ImponentText words={"MongoDB"} />.
+          </p>
+        </div>
+        <div className='absolute bottom-20 right-20 max-w-[600px] animate-4-bottom opacity-0 bg-black p-4 rounded-lg'>
+          <p className="text-left  text-neutral-200 text-xl">
+            I've created impactful projects, including <ImponentText words={"my-own-facebook"} /> and
+            an <ImponentText words={"animated 3D portfolio"} />, showcasing both technical expertise and
+            creativity. With over <ImponentText words={"900+ problems solved on LeetCode"} />, I continually
+            enhance my problem-solving abilities and stay eager to take on new challenges.
+          </p>
+        </div>
         <div className='absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 opacity-0 animate-5 text-neutral-800 text-2xl font-bold font-ubuntu'>
-          I am a Software <span className="line inline-block w-[20px] h-1 bg-black"></span>  Engineer
+          I am a Software <span className="line inline-block w-[20px] h-1 bg-black"></span> Engineer
         </div>
       </div>
     </div>
--- src/components/ui/imponent-text.jsx
+++ src/components/ui/imponent-text.jsx
@@ -2,6 +2,6 @@ import { cn } from "../../lib/utils";
 
 export const ImponentText = ({ words, className }) => {
     return (
-        <span className={cn("text-blue-400 font-bold text-[18px] tracking-wide", className)}>{words}</span>
+        <span className={cn("text-blue-500 font-bold text-[18px] tracking-wide", className)}>{words}</span>
     )
 }
