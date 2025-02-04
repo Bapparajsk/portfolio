eb82c7660482d68055a4fd4e321b1543a9bd9df2
Bapparaj sk <bapparajsk97@gmail.com>
2025-02-04 12:47:10 +0530

    feat: add background image to FireFliesBackground component for enhanced visual effect

--- src/components/FireFliesBackground.jsx
+++ src/components/FireFliesBackground.jsx
@@ -1,7 +1,18 @@
+import Image from "next/image";
+
 export const FireFliesBackground = () => {
     return (
         <div className="absolute top-0 left-0 w-full h-full z-10 overflow-hidden">
             <div className="w-screen h-screen bg-[#1a213a] shadow-[0_0_400px_84px_rgb(0,0,0)_inset]"/>
+            <div className="absolute top-1/2 -translate-y-1/2 right-[10%] w-auto h-auto z-10 overflow-hidden rounded-md">
+                <Image 
+                    src={"/themes-image/haunted-horror.webp"}
+                    width={1000}
+                    height={1000}
+                    alt="Horror House"
+                    className="object-cover opacity-5"
+                />
+            </div>
         </div>
     );
 }
