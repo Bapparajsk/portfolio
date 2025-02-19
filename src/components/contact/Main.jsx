4df0d75cf6a8180b0a8b10781e4c03872c93ffa3
Bapparaj sk <bapparajsk97@gmail.com>
2025-02-19 22:44:56 +0530

    feat: update ContactCard component to display 'Coming soon' message and adjust layout

--- src/components/contact/Main.jsx
+++ src/components/contact/Main.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 export default function ContactCard() {
   return (
-    <div className='w-full h-full border'>
-        
+    <div className='w-full h-full flex items-center justify-center'>
+        Coming soon
     </div>
   )
 }
