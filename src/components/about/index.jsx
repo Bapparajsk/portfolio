3a6fac89bcf6046cc802d2477c788646462ce161
Bapparaj sk <bapparajsk97@gmail.com>
2025-02-20 14:35:55 +0530

    feat: replace img with Image component for optimized loading in AboutDetails

--- src/components/about/index.jsx
+++ src/components/about/index.jsx
@@ -3,6 +3,7 @@
 import gsap from "gsap";
 import { useGSAP } from "@gsap/react";
 import { ScrollTrigger } from "gsap/all";
+import { Image } from "@/components/next";
 
 gsap.registerPlugin(ScrollTrigger);
 
@@ -41,7 +42,10 @@ export function AboutDetails() {
 
             <div className="h-dvh w-screen" id="clip">
                 <div className="mask-clip-path absolute left-1/2 top-0 z-20 h-[60vh] w-96 origin-center -translate-x-1/2 overflow-hidden rounded-3xl md:w-[30vw]">
-                    <img
+                    <Image
+                        width={1000}
+                        height={1000}
+                        loading="lazy"
                         src="/themes-image/haunted-house-about.png"
                         alt="Background"
                         className="absolute left-0 top-0 size-full object-cover"
